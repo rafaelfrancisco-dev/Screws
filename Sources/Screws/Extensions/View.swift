195b3d68be6087555a2580c908bc85d16af861c0
Rafael Francisco <rafaelfrancisco6@gmail.com>
2024-03-01 10:18:33 +0000

    Add a new conditional modifier extension

---
+++ Sources/Screws/Extensions/View.swift
@@ -0,0 +1,28 @@
+//
+//  View.swift
+//
+//
+//  Created by Rafael Francisco on 01/03/2024.
+//
+
+import SwiftUI
+
+@available(watchOS 6.0, *)
+@available(iOS 13.0, *)
+@available(tvOS 13.0, *)
+@available(macOS 10.15, *)
+@available(visionOS 1.0, *)
+extension View {
+    /// Applies the given transform if the given condition evaluates to `true`.
+    /// - Parameters:
+    ///   - condition: The condition to evaluate.
+    ///   - transform: The transform to apply to the source `View`.
+    /// - Returns: Either the original `View` or the modified `View` if the condition is `true`.
+    @ViewBuilder public func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
+}
